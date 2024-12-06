f1f460afa164a71dd39b3ab5cc479f97f4adecb0
Aya1302 <ayaelmasry64@gmail.com>
2024-12-06 14:17:50 +0200

    Add files via upload

---
+++ sec6/Form2.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace sec6
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::sec6.Properties.Resources._106;
+            this.pictureBox2.Location = new System.Drawing.Point(459, 87);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(245, 207);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::sec6.Properties.Resources.Ado;
+            this.pictureBox1.Location = new System.Drawing.Point(129, 87);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(226, 204);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ sec6/Form3.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sec6
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+        private void Form3_Load_1(object sender, EventArgs e)
+        {
+            display();
+        }
+        
+
+        private void display()
+        {
+            SqlConnection con = new SqlConnection("Data Source=aya;Initial Catalog=scool2;Integrated Security=True;TrustServerCertificate=True;");
+            con.Open();
+            DataTable dt = new DataTable();
+            SqlDataAdapter adapt = new SqlDataAdapter("select * from student", con);
+            adapt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void insert()
+        {
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+            {
+                SqlConnection con = new SqlConnection("Data Source=aya;Initial Catalog=scool2;Integrated Security=True;TrustServerCertificate=True;");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert into student(id,name,age) values(@id,@name,@age)", con);
+                cmd.Parameters.AddWithValue("@id", textBox1.Text);
+                cmd.Parameters.AddWithValue("@name", textBox2.Text);
+                cmd.Parameters.AddWithValue("@age", textBox3.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record Inserted Successfully");
+            }
+            else
+            {
+                MessageBox.Show("please provide details");
+            }
+        }
+
+
+        private void delete()
+        {
+            if (textBox1.Text != "")
+            {
+                SqlConnection con = new SqlConnection("Data Source=aya;Initial Catalog=scool2;Integrated Security=True;TrustServerCertificate=True;");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("delete from student where id=@id", con);
+                cmd.Parameters.AddWithValue("@id", textBox1.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record deleted Successfully");
+            }
+            else
+            {
+                MessageBox.Show("please enter id to delete");
+            }
+        }
+       
+ 
+
+
+        private void update()
+        {
+            if (textBox1.Text != "")
+            {
+                SqlConnection con = new SqlConnection("Data Source=aya;Initial Catalog=scool2;Integrated Security=True;TrustServerCertificate=True;");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("update student set name=@name, age=@age where id=@id", con);
+                cmd.Parameters.AddWithValue("@id", textBox1.Text);
+                cmd.Parameters.AddWithValue("@name", textBox2.Text);
+                cmd.Parameters.AddWithValue("@age", textBox3.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record updated Successfully");
+            }
+            else
+            {
+                
+                MessageBox.Show("please enter details to update");
+            }
+        }
+
+        private void clear()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            insert();
+            display();
+            clear();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            delete();
+            display();
+            clear();
+        }
+       
+        private void button3_Click(object sender, EventArgs e)
+        {
+            update();
+            display();
+            clear();
+        }
+
+
+
+
+
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
